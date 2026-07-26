e0db3a38d020d0ae9752e88a5c7064ca46b553e8
Grey <xandergrey25@gmail.com>
2026-07-26 16:45:57 +0800

    completed 03-functions-exercises-2.js

--- phase-1/03-functions-exercises-2.js
+++ phase-1/03-functions-exercises-2.js
@@ -4,3 +4,20 @@
 // 3. Returns the total
 // 4. Create an array of 3-4 products and test it
 // Example: [{ price: 100 }, { price: 200 }] → returns 300
+
+const calculateCartTotal = (products) => {
+    let sum = 0;
+    for (let i = 0; i < products.length; i++) {
+        sum += products[i].price;
+    }
+    return sum;
+}
+
+cartProducts = [
+    { price: 100 },
+    { price: 200 },
+    { price: 300 },
+    { price: 400 },
+];
+
+console.log(calculateCartTotal(cartProducts)); // 1000
