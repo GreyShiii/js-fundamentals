64a4bbff772ad986e113bc1559b0d5d8d8aca3c4
Grey <xandergrey25@gmail.com>
2026-07-26 17:02:14 +0800

    completed 03-functions-exercises-3.js

--- phase-1/03-functions-exercises-3.js
+++ phase-1/03-functions-exercises-3.js
@@ -6,3 +6,18 @@
 // Example: applyDiscount(1000, 20) → returns 800
 
 // Bonus: Use short-form arrow function (implicit return)
+
+// const applyDiscount = (price, discountPercent) => {
+//     const discountAmount = price * (discountPercent / 100);
+//     const finalPrice = price - discountAmount;
+//     return finalPrice;
+// }
+
+const applyDiscount = (price, discountPercent) => price - (price * (discountPercent / 100)); // short-form arrow function
+
+console.log(applyDiscount(1000, 20)); // 800
+console.log(applyDiscount(500, 80)); // 100
+console.log(applyDiscount(100, 50)); // 50
+
+
+
