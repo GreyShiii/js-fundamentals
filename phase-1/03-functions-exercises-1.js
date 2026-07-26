e6c339931161be14b0590fa7df989783f59e74a4
Grey <xandergrey25@gmail.com>
2026-07-26 16:28:09 +0800

    completed 03-functions-exercises-1.js

--- phase-1/03-functions-exercises-1.js
+++ phase-1/03-functions-exercises-1.js
@@ -5,3 +5,19 @@
 //    - "Exactly your budget" if price === maxBudget
 //    - "Too expensive" if price > maxBudget
 // 3. Call it with at least 3 different test cases
+
+const validatePrice = (price, maxBudget) => {
+    if (price < maxBudget) {
+        return "You can afford it";
+    } else if (price === maxBudget) {
+        return "Exaclty your budget";
+    } else {
+        return "Too expensive"
+    }
+}
+
+console.log(validatePrice(500, 550)); // you can afford it
+console.log(validatePrice(500, 500)); // Exactly your budget
+console.log(validatePrice(500, 430)); // Too expensive
+
+
