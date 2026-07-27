8bffee508770930f93211752de6188bd7e61d48d
Grey <xandergrey25@gmail.com>
2026-07-27 19:34:50 +0800

    completed 04-array-exercises-1.js

--- phase-1/04-array-exercises-1.js
+++ phase-1/04-array-exercises-1.js
@@ -4,3 +4,15 @@
 // 3. Use map() to get just the names of those products
 // 4. Log the result
 // Example output: ["Laptop", "Mouse", "Keyboard"]
+
+const products = [
+    {name: "Mouse", price: 599, category: "Electronics"},
+    {name: "Keyboard", price: 899, category: "Electronics"},
+    {name: "Chair", price: 299, category: "Houseware"},
+];
+
+const electronicProducts = products
+    .filter((product) => product.category === "Electronics")
+    .map((product) => product.name);
+console.log("Electronics list:", electronicProducts);
+
