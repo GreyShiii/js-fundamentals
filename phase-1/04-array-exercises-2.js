a2774fdc1a184d706b3e68a767f5d80c7c963ecf
Grey <xandergrey25@gmail.com>
2026-07-27 19:49:08 +0800

    completed 04-array-exercises-2.js

--- phase-1/04-array-exercises-2.js
+++ phase-1/04-array-exercises-2.js
@@ -3,3 +3,12 @@
 // 2. Use find() to search for "Mouse"
 // 3. Use includes() to check if "Laptop" is in each name
 // 4. Log results showing which product was found and which names include "Laptop"
+
+const products = ["Laptop", "Mouse", "Keyboard", "Monitor"];
+const mouseSearch = products.find((product) => product === "Mouse"); // Finding mouse
+
+console.log("Found product:", mouseSearch);
+
+products.forEach((product) => {
+    console.log(`Does "${product}" include "Laptop?"`, product.includes("Laptop"));
+});
