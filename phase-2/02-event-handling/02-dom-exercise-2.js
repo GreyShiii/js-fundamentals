db21aacd157f17bb658038ff01ef971d51323258
Grey <xandergrey25@gmail.com>
2026-07-31 21:05:01 +0800

    completed phase-2/02-dom-exercise-2

--- phase-2/02-event-handling/02-dom-exercise-2.js
+++ phase-2/02-event-handling/02-dom-exercise-2.js
@@ -13,3 +13,25 @@
 // If valid, show: "✓ Added: [name] x[quantity]" and clear the inputs
 // Test: Page should NEVER refresh when you submit
 // Hint: Use event.target inside the form listener to get the form itself. Access inputs with getElementById().
+
+const productForm = document.getElementById("productForm");
+const result = document.getElementById("result");
+
+productForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const productName = event.target.name.value.trim();
+    const productQuantity = event.target.quantity.value.trim();
+
+    if (!productName) {
+        result.textContent = `Error: Item name is required!`;
+        return;
+    }
+
+    if (!productQuantity || productQuantity <= 0) {
+        result.textContent = `Error: Quantity must be at least 1!`;
+        return;
+    }
+
+    result.textContent = `Added: ${productName} ${productQuantity}`;
+    event.target.reset();
+});
