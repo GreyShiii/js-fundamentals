94ecafc24efb87cbc5e9a909e03a005d5fa3baee
Grey <xandergrey25@gmail.com>
2026-08-01 14:18:56 +0800

    completed phase-2/02-dom-exercise-4

--- phase-2/02-event-handling/02-dom-exercise-4.js
+++ phase-2/02-event-handling/02-dom-exercise-4.js
@@ -9,3 +9,20 @@
 // If search box is empty, hide all products
 // Log: "Searching for: [query]"
 // // Hint: Add/remove a CSS class like .visible to show/hide products. Use .toLowerCase().includes() to check if a product name matches.
+
+const inputSearch = document.getElementById("inputSearch");
+const products = document.querySelectorAll(".products");
+
+inputSearch.addEventListener("input", (event) => {
+    const inputResult = event.target.value.toLowerCase();
+    
+    products.forEach((product) => {
+        const productName = product.getAttribute("data-product").toLowerCase();
+
+        if (productName.includes(inputResult) && inputResult !== "") {
+            product.classList.add("visible");
+        } else {
+            product.classList.remove("visible");
+        }
+    });
+});
