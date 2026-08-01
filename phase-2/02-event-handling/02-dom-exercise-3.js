816774f84f39f6bb1dbfef30e08ab75060dcb3c7
Grey <xandergrey25@gmail.com>
2026-08-01 12:59:21 +0800

    completed phase-2/02-dom-exercise-3

--- phase-2/02-event-handling/02-dom-exercise-3.js
+++ phase-2/02-event-handling/02-dom-exercise-3.js
@@ -9,3 +9,35 @@
 // Log: "Deleted: [item text]"
 // Bonus: Click "Add New Item" to add a new item dynamically. The delete button should work automatically (thanks to event delegation!)
 // Hint: Use event.target.classList.contains("delete-btn") to check if a delete button was clicked. Use closest() to get the item div: event.target.closest(".item").
+
+const productContainer = document.getElementById("productContainer");
+const addNewBtn = document.getElementById("addNewBtn");
+
+productContainer.addEventListener("click", (event) => {
+    if (event.target.classList.contains("delete-btn")) {
+        const cartItem = event.target.closest(".cart-item");
+        const productName = cartItem.querySelector("span").textContent;
+
+        cartItem.remove();
+        console.log(`Deleted: ${productName}`);
+    }
+});
+
+const productLists = [
+    { name:"Monitor" },
+    { name:"Headset" },
+    { name:"Earphones" }
+]
+
+addNewBtn.addEventListener("click", (event) => {
+    const randomProduct = productLists[Math.floor(Math.random() * productLists.length)];
+
+    const newProduct = document.createElement("div");
+    newProduct.className = "cart-item";
+    newProduct.innerHTML = `
+    <span>${randomProduct.name}</span>
+    <button class="delete-btn">Delete</button>
+    `;
+
+    productContainer.appendChild(newProduct);
+});
