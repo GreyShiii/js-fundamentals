7c991b811cdbfa59e9ea0b4580aca7535c572ab4
Grey <xandergrey25@gmail.com>
2026-08-04 14:41:09 +0800

    completed phase-3/02-async-await-exercise-3

--- Phase-3/02-async-await/02-async-await-exercise-3.js
+++ Phase-3/02-async-await/02-async-await-exercise-3.js
@@ -20,3 +20,45 @@
 // Fetch user first, then fetch posts
 // Both inside the same try block
 // Use .substring(0, 40) to limit title length
+
+const loadCompleteData = document.getElementById("loadCompleteData");
+const output = document.getElementById("output");
+
+async function loadData() {
+    output.innerHTML = "<p class='loading'>Loading products...</p>";
+
+    try {
+        const userResponse = await fetch("https://jsonplaceholder.typicode.com/users/2");
+        if (!userResponse.ok) {
+            throw new Error(`HTTP Response status error: ${userResponse.status}`);
+        }
+        const user = await userResponse.json();
+
+        const postsResponse = await fetch("https://jsonplaceholder.typicode.com/posts?userId=2&_limit=4");
+        if (!postsResponse.ok) {
+            throw new Error(`HTTP Response status error: ${postsResponse.status}`);
+        }
+        const posts = await postsResponse.json();
+
+        // Display
+        output.innerHTML = "";
+
+        const div = document.createElement("div");
+        div.className = "success";
+        div.innerHTML = `
+            <p>Name: ${user.name}</p><br>
+            <p>Email: ${user.email}</p><br>
+            <p>Number of posts: ${posts.length}</p><br>
+            <p>Title: ${posts[0].title.substring(0, 40)}</p><br>
+        `;
+        output.appendChild(div);
+        console.log("User:", user);
+        console.log("Posts:", posts);
+        
+    } catch(error) {
+        output.innerHTML = `<p class='error'>${error.message}</p>`;
+        console.log("Error:", error);
+    }
+}
+
+loadCompleteData.addEventListener("click", loadData);
