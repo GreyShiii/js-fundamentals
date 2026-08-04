4f8e8bacf8d53292f4a57e63a14de6acbadfe29e
Grey <xandergrey25@gmail.com>
2026-08-04 13:51:04 +0800

    completed phase-3/02-async-await-exercise-1

--- Phase-3/02-async-await/02-async-await-exercise-1.js
+++ Phase-3/02-async-await/02-async-await-exercise-1.js
@@ -10,3 +10,29 @@
 // Display the post ID, title, and body
 // Test: Click button and see post appear
 // Log the data to console
+
+const loadPost = document.getElementById("loadPost");
+const output = document.getElementById("output");
+
+loadPost.addEventListener("click", async () => {
+    output.className = "loading";
+    output.textContent = "Loading...";
+
+    try {
+    const postsResponse = await fetch("https://jsonplaceholder.typicode.com/posts/2");
+    const posts = await postsResponse.json();
+
+    output.className = "";
+    output.innerHTML = `
+        <p>ID: ${posts.id}</p><br>
+        <p>Title: ${posts.title}</p><br>
+        <p>Body: ${posts.body}</p><br>
+    `;
+    console.log("Result:", posts);
+    
+    } catch (error) {
+        output.className = "error";
+        output.textContent = `Error: ${error.message}`;
+        console.log("Error:", error);
+    }
+});
